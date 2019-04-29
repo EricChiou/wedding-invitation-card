0379abf984516834354b380502f97ffef0076f4f
eric.chiou <eric.chiou@acer.com>
2019-04-29 19:26:59 +0800

    detect webview

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,29 @@
   </div>
 </template>
 
+<script>
+export default {
+  methods: {
+    detectWebView() {
+      if (window.navigator.userAgent) {
+        if (/(FBAV|Line|wv|IAB|WebView)/.test(window.navigator.userAgent)) {
+          if (/(Android|android)/.test(window.navigator.userAgent)) {
+            window.location.href =
+              'googlechrome://navigate?url=www.calicomoo.ml/wedding';
+          } else if (/(iPod|iPad|iPhone)/.test(window.navigator.userAgent)) {
+            window.location.href = 'googlechrome://www.calicomoo.ml/wedding';
+          }
+        }
+      }
+    }
+  },
+  created() {
+    this.detectWebView();
+  }
+};
+</script>
+
+
 <style lang="scss">
 html,
 body {
