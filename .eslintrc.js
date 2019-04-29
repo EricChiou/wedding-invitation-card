411e036a93eaaaaa28288e9e1f0a6e8456624710
EricChiou <miatlab@gmail.com>
2019-04-29 11:39:32 +0800

    first commit

--- .eslintrc.js
+++ .eslintrc.js
@@ -10,6 +10,11 @@ module.exports = {
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'comma-dangle': ['error', 'never'],
+    'no-param-reassign': [2, { props: false }],
+    'operator-linebreak': ['error', 'after'],
+    'linebreak-style': ['off'],
+    'arrow-parens': ['off']
   },
   parserOptions: {
     parser: 'babel-eslint',
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,159 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="content">
+      <div class="start">
+        <img src="../assets/images/page_1.jpg">
+      </div>
+      <div class="picture-container">
+        <div class="picture">
+          <img src="../assets/images/picture.jpg">
+        </div>
+        <img src="../assets/images/page_2.jpg">
+      </div>
+      <div class="end">
+        <img src="../assets/images/page_3.jpg">
+      </div>
+    </div>
+    <div class="cover">
+      <div class="middle"></div>
+      <img src="../assets/images/cover.jpg">
+    </div>
+    <div class="sound" @click="sound()">
+      <img src="../assets/images/sound.svg">
+      <div class="close"></div>
+      <audio id="bgm" controls hidden :src="bgmUrl" @canplay="play()"></audio>
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue';
-
 export default {
   name: 'home',
-  components: {
-    HelloWorld,
+  components: {},
+  data: () => ({ bgmUrl: '' }),
+  methods: {
+    animation() {
+      const cover = document.getElementsByClassName('cover')[0];
+      const content = document.getElementsByClassName('content')[0];
+      this.fade(cover, content, 1);
+    },
+    fade(cover, content, opacity) {
+      cover.style.opacity = opacity;
+      content.style.opacity = 1 - opacity;
+      if (opacity > 0) {
+        setTimeout(() => {
+          this.fade(cover, content, opacity - 0.05);
+        }, 60);
+      } else {
+        window.scrollTo({ left: 0, top: 0, behavior: 'smooth' });
+      }
+    },
+    sound() {
+      const sound = document.getElementsByClassName('close')[0];
+      if (sound.style.display === 'block') {
+        sound.style.display = 'none';
+        document.getElementById('bgm').play();
+      } else {
+        sound.style.display = 'block';
+        document.getElementById('bgm').pause();
+      }
+    },
+    play() {
+      const playPromise = document.getElementById('bgm').play();
+      playPromise
+        .then(() => {})
+        .catch(error => {
+          console.log(error);
+          this.sound();
+        });
+    },
+    getBgmUrl() {
+      if (
+        window.location.host.indexOf('localhost') > -1 ||
+        window.location.host.indexOf('192.168.0.103') > -1
+      ) {
+        this.bgmUrl = 'http://192.168.0.103:8080/wedding/bgm.mp3';
+      } else {
+        this.bgmUrl = 'http://www.calicomoo.ml:8080/wedding/bgm.mp3';
+      }
+    }
   },
+  created() {
+    this.getBgmUrl();
+  },
+  mounted() {
+    setTimeout(() => {
+      this.animation();
+    }, 1500);
+  }
 };
 </script>
+
+<style lang="scss" scoped>
+.content {
+  .start {
+    margin: 10px 0 0 10px;
+  }
+  .picture-container {
+    display: inline-block;
+    position: relative;
+    width: 1200px;
+    max-width: 100%;
+    .picture {
+      position: absolute;
+      top: 6%;
+      right: 11%;
+      left: 11%;
+    }
+  }
+  img {
+    display: inline-block;
+    max-width: 100%;
+  }
+  .end {
+    img {
+      max-width: 40%;
+    }
+  }
+}
+.sound {
+  position: fixed;
+  top: 0;
+  right: 2px;
+  opacity: 0.8;
+  cursor: pointer;
+  img {
+    width: 30px;
+    height: 30px;
+  }
+  .close {
+    display: none;
+    border-top: 2px solid #888888;
+    border-bottom: 2px solid #888888;
+    border-radius: 2px;
+    position: fixed;
+    top: 13px;
+    right: 0;
+    width: 35px;
+    transform: rotate(315deg);
+  }
+}
+.cover {
+  position: fixed;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  left: 0;
+  background-color: #d08490;
+  .middle {
+    display: inline-block;
+    height: 100%;
+    vertical-align: middle;
+  }
+  img {
+    display: inline-block;
+    max-width: 100%;
+    vertical-align: middle;
+  }
+}
+</style>
